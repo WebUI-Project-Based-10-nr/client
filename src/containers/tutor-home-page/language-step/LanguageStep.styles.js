ff27848d27412abf9983d3f6b8ea7f97bf45a067
Kateryna Bartienieva <108666032+k7bart@users.noreply.github.com>
2024-10-28 16:17:04 +0200

    Implement language selection (#163)
    * correct text
    
    * add language step constants
    
    * move image to separate component
    
    * implement language selection

--- src/containers/tutor-home-page/language-step/LanguageStep.styles.js
+++ src/containers/tutor-home-page/language-step/LanguageStep.styles.js
@@ -8,17 +8,6 @@ export const styles = {
     height: { sm: '485px' },
     ...fadeAnimation
   },
-  imgContainer: {
-    display: 'flex',
-    flex: 1,
-    maxWidth: '432px',
-    aspectRatio: { xs: '4/3', sm: 'auto' },
-    pb: { xs: '16px', sm: '52px' }
-  },
-  img: {
-    width: '100%',
-    m: { sm: 0, xs: '0 auto' }
-  },
   rigthBox: {
     maxWidth: '432px',
     display: 'flex',

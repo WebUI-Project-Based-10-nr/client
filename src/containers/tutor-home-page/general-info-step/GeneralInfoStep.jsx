7928674c36f2a5837cef19e6354aaeaca6335f88
geersann <b.kavyn2000@gmail.com>
2024-10-28 04:24:06 +0200

    use api for render countries and cities in general step

--- src/containers/tutor-home-page/general-info-step/GeneralInfoStep.jsx
+++ src/containers/tutor-home-page/general-info-step/GeneralInfoStep.jsx
@@ -1,21 +1,179 @@
-import { Typography } from '@mui/material'
-import Box from '@mui/material/Box'
+import { useEffect, useState } from 'react'
+import { Typography, Box, Grid } from '@mui/material'
 import { useTranslation } from 'react-i18next'
-
+import AppTextField from '~/components/app-text-field/AppTextField'
+import AppTextArea from '~/components/app-text-area/AppTextArea'
 import { styles } from '~/containers/tutor-home-page/general-info-step/GeneralInfoStep.styles'
+import illustration from '~/assets/img/tutor-home-page/become-tutor/general-info.svg'
+import { createFilterOptions } from '@mui/material'
+import AppAutoComplete from '~/components/app-auto-complete/AppAutoComplete'
+import axios from 'axios'
+import { useStepContext } from '~/context/step-context'
 
 const GeneralInfoStep = ({ btnsBox }) => {
   const { t } = useTranslation()
+  const { stepData, handleGeneralInfo } = useStepContext()
+  const generalInfo = stepData.generalInfo
+
+  const [countries, setCountries] = useState([])
+  const [countryCodes, setCountryCodes] = useState({})
+  const [cities, setCities] = useState([])
+  const [data, setData] = useState({
+    firstName: generalInfo.data.firstName || '',
+    lastName: generalInfo.data.lastName || '',
+    country: generalInfo.data.country || '',
+    city: generalInfo.data.city || '',
+    professionalSummary: generalInfo.data.professionalSummary || ''
+  })
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then((response) => {
+        const countryNames = response.data
+          .map((country) => {
+            setCountryCodes((prevCodes) => ({
+              ...prevCodes,
+              [country.name.common]: country.cca2
+            }))
+            return country.name.common
+          })
+          .sort((a, b) => a.localeCompare(b))
+        setCountries(countryNames)
+      })
+      .catch((error) => {
+        console.error('Error fetching countries:', error)
+      })
+  }, [])
+
+  const fetchCities = (country) => {
+    const countryCode = countryCodes[country]
+
+    if (countryCode) {
+      axios
+        .get(
+          `http://api.geonames.org/searchJSON?formatted=true&country=${countryCode}&maxRows=10&username=geer_sann`
+        )
+        .then((response) => {
+          console.log(response.data)
+          const cityNames = response.data.geonames.map((city) => city.name)
+          setCities(cityNames)
+        })
+        .catch((error) => {
+          console.error('Error fetching cities:', error)
+        })
+    } else {
+      setCities([])
+    }
+  }
+
+  useEffect(() => {
+    if (data.country) {
+      fetchCities(data.country)
+    } else {
+      setCities([])
+    }
+  }, [data.country])
+
+  useEffect(() => {
+    handleGeneralInfo({ data })
+  }, [data, handleGeneralInfo])
+
+  const handleInputChange = (field) => (event) => {
+    setData({ ...data, [field]: event.target.value })
+  }
+
+  const filterOptions = (options, state) => {
+    const defaultFilterOptions = createFilterOptions()
+    return defaultFilterOptions(options, state).slice(0, 300)
+  }
+
   return (
     <Box sx={styles.container}>
-      <Box component='form' sx={styles.form}>
-        <Box>
+      <Grid alignItems='center' container justifyContent='center' spacing={4}>
+        <Grid item md={6} sx={{ display: { xs: 'none', md: 'block' } }} xs={12}>
+          <Box sx={styles.imgContainer}>
+            <Box
+              alt='Illustration'
+              component='img'
+              src={illustration}
+              sx={{ maxWidth: '100%', height: 'auto' }}
+            />
+          </Box>
+        </Grid>
+        <Box component='form' sx={styles.form}>
           <Typography mb='20px'>
             {t('becomeTutor.generalInfo.title')}
           </Typography>
+          <Box sx={styles.formFieldsContainer}>
+            <AppTextField
+              fullWidth
+              label={t('common.labels.firstName')}
+              onBlur={() => {}}
+              onChange={handleInputChange('firstName')}
+              placeholder={t('common.labels.firstName')}
+              required
+              sx={{ mb: '5px' }}
+              type='text'
+              value={data.firstName}
+            />
+            <AppTextField
+              fullWidth
+              label={t('common.labels.lastName')}
+              onBlur={() => {}}
+              onChange={handleInputChange('lastName')}
+              placeholder={t('common.labels.lastName')}
+              required
+              sx={{ mb: '5px' }}
+              type='text'
+              value={data.lastName}
+            />
+            <AppAutoComplete
+              onChange={(event, newValue) => {
+                setData((prevData) => ({
+                  ...prevData,
+                  country: newValue,
+                  city: ''
+                }))
+              }}
+              options={countries}
+              sx={{ mb: '30px' }}
+              textFieldProps={{
+                label: t('common.labels.country')
+              }}
+              value={data.country}
+            />
+            <AppAutoComplete
+              disabled={!data.country}
+              filterOptions={filterOptions}
+              onChange={(event, newValue) => {
+                setData((prevData) => ({
+                  ...prevData,
+                  city: newValue
+                }))
+              }}
+              options={cities}
+              sx={{ mb: '30px' }}
+              textFieldProps={{
+                label: t('common.labels.city')
+              }}
+              value={data.city}
+            />
+          </Box>
+          <AppTextArea
+            fullWidth
+            label={t('becomeTutor.generalInfo.textFieldLabel')}
+            maxLength={70}
+            onChange={handleInputChange('professionalSummary')}
+            type='text'
+            value={data.professionalSummary}
+          />
+          <Typography mb='20px'>
+            {t('becomeTutor.generalInfo.helperText')}
+          </Typography>
+          {btnsBox}
         </Box>
-        {btnsBox}
-      </Box>
+      </Grid>
     </Box>
   )
 }

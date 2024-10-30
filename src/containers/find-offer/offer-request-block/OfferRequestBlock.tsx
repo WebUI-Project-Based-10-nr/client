043a1cea3469138c68cbcd2cde8a4d3826a728e5
Kateryna Bartienieva <k7bart@gmail.com>
2024-10-30 21:47:38 +0200

    implement "Create offer/request" block

--- src/containers/find-offer/offer-request-block/OfferRequestBlock.tsx
+++ src/containers/find-offer/offer-request-block/OfferRequestBlock.tsx
@@ -1,29 +1,30 @@
 import { useTranslation } from 'react-i18next'
+import { useAppSelector } from '~/hooks/use-redux'
+import { UserRoleEnum } from '~/types'
+import useBreakpoints from '~/hooks/use-breakpoints'
 
+import AppButton from '~/components/app-button/AppButton'
 import TitleBlock from '~/components/title-block/TitleBlock'
 import icon from '~/assets/img/find-offer/subject_icon.png'
-import AppButton from '~/components/app-button/AppButton'
-import useBreakpoints from '~/hooks/use-breakpoints'
-import { useDrawer } from '~/hooks/use-drawer'
-import { translationKey } from '~/containers/find-offer/constants'
+
+const translationKey = 'findOffers.offerRequestBlock'
 
 const OfferRequestBlock = () => {
   const { t } = useTranslation()
   const { isMobile } = useBreakpoints()
-  const { openDrawer } = useDrawer()
 
-  const handleOpenDrawer = () => {
-    openDrawer()
-  }
+  const userRole = useAppSelector((state) => state.appMain.userRole)
+  const buttonText =
+    userRole === UserRoleEnum.Student
+      ? `${translationKey}.button.student`
+      : `${translationKey}.button.tutor`
+
+  const openPopup = () => {} // TODO: add "Create offer/request" popup
 
   return (
     <TitleBlock img={icon} translationKey={translationKey}>
-      <AppButton
-        fullWidth={isMobile}
-        onClick={handleOpenDrawer}
-        sx={{ py: '14px' }}
-      >
-        {t(`${translationKey}.button`)}
+      <AppButton fullWidth={isMobile} onClick={openPopup} sx={{ py: '14px' }}>
+        {t(buttonText)}
       </AppButton>
     </TitleBlock>
   )

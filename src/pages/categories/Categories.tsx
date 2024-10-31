3380c9352c3197f710bcf0fb17cf0086232413cd
Annaivv <annashalya@gmail.com>
2024-10-31 14:30:37 +0200

    CategoriesList rendered

--- src/pages/categories/Categories.tsx
+++ src/pages/categories/Categories.tsx
@@ -1,7 +1,12 @@
+import CategoriesList from '~/components/categories-list/CategoriesList'
 import PageWrapper from '~/components/page-wrapper/PageWrapper'
 
 const Categories = () => {
-  return <PageWrapper>Categories</PageWrapper>
+  return (
+    <PageWrapper>
+      <CategoriesList />
+    </PageWrapper>
+  )
 }
 
 export default Categories

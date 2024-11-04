00a8a05b6739e3ec74bf6d0df23dbc0c1f2b5f1f
Annaivv <annashalya@gmail.com>
2024-11-04 21:31:59 +0200

    icons added to the category cards

--- src/pages/categories/Categories.tsx
+++ src/pages/categories/Categories.tsx
@@ -1,11 +1,12 @@
-import { useCallback, useMemo } from 'react'
+import React, { useCallback, useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import { useModalContext } from '~/context/modal-context'
 import useBreakpoints from '~/hooks/use-breakpoints'
 import useLoadMore from '~/hooks/use-load-more'
 
-import serviceIcon from '~/assets/img/student-home-page/service_icon.png'
+//import { SvgIconProps } from '@mui/material'
+//import serviceIcon from '~/assets/img/student-home-page/service_icon.png'
 import { itemsLoadLimit } from '~/constants'
 import { authRoutes } from '~/router/constants/authRoutes'
 import { CategoryInterface } from '~/types'
@@ -16,8 +17,9 @@ import NotFoundResults from '~/components/not-found-results/NotFoundResults'
 import PageWrapper from '~/components/page-wrapper/PageWrapper'
 import CardWithLink from '~/components/card-with-link/CardWithLink'
 import CardsList from '~/components/cards-list/CardsList'
+import { categoryIcons } from '~/constants/icon-map'
 
-// const categories = null
+type CategoryIconKey = keyof typeof categoryIcons
 
 const Categories = () => {
   const { t } = useTranslation()
@@ -44,10 +46,30 @@ const Categories = () => {
   const cards = useMemo(
     () =>
       categories.map((item: CategoryInterface) => {
+        const defaultIcon =
+          'https://www.svgrepo.com/show/508699/landscape-placeholder.svg'
+        const iconDb = item.appearance.icon
+        const iconDbUrl = iconDb && iconDb.includes('http')
+        const IconComponent = categoryIcons[item.name as CategoryIconKey]
+
+        const renderedIcon = iconDbUrl ? (
+          iconDb
+        ) : IconComponent ? (
+          <IconComponent
+            fontSize='large'
+            sx={{
+              color: item.appearance.color || '#808080',
+              paddingRight: '12px'
+            }}
+          />
+        ) : (
+          defaultIcon
+        )
+
         return (
           <CardWithLink
             description={`16 ${t('categoriesPage.offers')}`}
-            img={serviceIcon}
+            img={renderedIcon}
             key={item._id}
             link={`${authRoutes.subjects.path}?categoryId=${item._id}`}
             title={item.name}

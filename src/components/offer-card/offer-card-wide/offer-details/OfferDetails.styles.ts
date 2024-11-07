f098d8fc856ad6a859e9ea3fa466780e107bf448
Kateryna Bartienieva <k7bart@gmail.com>
2024-11-07 21:33:47 +0200

    create OfferDetails component, add styles

---
+++ src/components/offer-card/offer-card-wide/offer-details/OfferDetails.styles.ts
@@ -0,0 +1,17 @@
+export const styles = {
+  title: {
+    color: 'primary.700'
+  },
+  chipContainer: {
+    py: '10px'
+  },
+  description: {
+    color: 'primary.600',
+    display: '-webkit-box',
+    WebkitLineClamp: 4,
+    lineClamp: 4,
+    WebkitBoxOrient: 'vertical',
+    overflow: 'hidden',
+    mb: '10px'
+  }
+}
---
+++ src/components/offer-card/offer-card-wide/offer-details/OfferDetails.tsx
@@ -0,0 +1,43 @@
+import { Box, Typography } from '@mui/material'
+import { LanguagesEnum, ProficiencyLevelEnum } from '~/types'
+import { styles } from '~/components/offer-card/offer-card-wide/offer-details/OfferDetails.styles'
+import LanguagesListWithIcon from '~/components/languages-list-with-icon/LanguagesListWithIcon'
+import SubjectLevelChips from '~/components/subject-level-chips/SubjectLevelChips'
+
+interface OfferDetailsProps {
+  details: {
+    chipsColor: string
+    description: string
+    languages: LanguagesEnum | LanguagesEnum[]
+    level: ProficiencyLevelEnum | ProficiencyLevelEnum[]
+    subject: string
+    title: string
+  }
+}
+
+const OfferDetails: React.FC<OfferDetailsProps> = ({ details }) => {
+  const { chipsColor, description, languages, level, subject, title } = details
+
+  return (
+    <Box sx={{ flex: 1 }}>
+      <Typography sx={styles.title} variant='h6'>
+        {title}
+      </Typography>
+
+      <SubjectLevelChips
+        color={chipsColor}
+        proficiencyLevel={level}
+        subject={subject}
+        sx={styles.chipContainer}
+      />
+
+      <Typography sx={styles.description} variant='body2'>
+        {description}
+      </Typography>
+
+      <LanguagesListWithIcon languages={languages} />
+    </Box>
+  )
+}
+
+export default OfferDetails

ee4acf311e7de550f0c3ba0c5f1c472921ecc3c2
Kateryna Bartienieva <k7bart@gmail.com>
2024-11-07 23:05:38 +0200

    create OfferCardSquare component, add styles

---
+++ src/components/offer-card/offer-card-square/OfferCardSquare.tsx
@@ -0,0 +1,94 @@
+import { Box, Divider, Typography } from '@mui/material'
+import { Offer } from '~/types'
+import { styles } from './OfferCardSquare.styles'
+
+import AppCard from '~/components/app-card/AppCard'
+import AppRatingMobile from '~/components/app-rating-mobile/AppRatingMobile'
+import Bookmark from '~/components/offer-card/bookmark/Bookmark'
+import Price from '~/components/offer-card/price/Price'
+import SendMessageButton from '~/components/offer-card/buttons/SendMessageButton'
+import SubjectLevelWithlabels from '~/components/subject-level-with-labels/SubjectLevelWithLabels'
+import UserProfileInfo from '~/components/user-profile-info/UserProfileInfo'
+import ViewDetailsButton from '~/components/offer-card/buttons/ViewDetailsButton'
+
+interface OfferCardProps {
+  offer: Offer
+  onBookmarkClick: (id: string) => void
+  isBookmarked: boolean
+}
+
+const OfferCardSquare: React.FC<OfferCardProps> = ({
+  offer,
+  onBookmarkClick,
+  isBookmarked
+}) => {
+  const {
+    _id,
+    title,
+    languages,
+    price,
+    author,
+    authorRole,
+    subject,
+    category,
+    proficiencyLevel
+  } = offer
+
+  const userProfileInfoProps = {
+    _id: author._id,
+    firstName: author.firstName,
+    languages,
+    lastName: author.lastName,
+    photo: author.photo,
+    role: authorRole,
+    sx: styles.userInfo
+  }
+
+  const bookmarkProps = {
+    id: _id,
+    isBookmarked,
+    onBookmarkClick
+  }
+
+  // TODO: add chat opening logic
+  const handleChatOpening = () => {}
+
+  return (
+    <AppCard sx={styles.appCard}>
+      <Box sx={styles.container}>
+        <Bookmark {...bookmarkProps} />
+
+        <UserProfileInfo {...userProfileInfoProps} />
+
+        <Typography sx={styles.description}>{title}</Typography>
+
+        <Divider />
+
+        <SubjectLevelWithlabels
+          color={category.appearance.color}
+          proficiencyLevel={proficiencyLevel}
+          subject={subject.name}
+        />
+
+        <Box sx={styles.cardContent}>
+          <Box sx={styles.priceContainer}>
+            <Price price={price} />
+
+            <AppRatingMobile
+              reviewsCount={author.totalReviews[authorRole]}
+              value={author.averageRating[authorRole]}
+            />
+          </Box>
+
+          <Box sx={styles.buttonContainer}>
+            <ViewDetailsButton />
+
+            <SendMessageButton onClick={handleChatOpening} />
+          </Box>
+        </Box>
+      </Box>
+    </AppCard>
+  )
+}
+
+export default OfferCardSquare

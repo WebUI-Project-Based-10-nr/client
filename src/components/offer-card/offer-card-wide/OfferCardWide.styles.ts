70a96ffac92e7c699880fe5e6ce18ecd93c6c2d3
Kateryna Bartienieva <k7bart@gmail.com>
2024-11-07 22:52:15 +0200

    create OfferCardWide component, add styles

---
+++ src/components/offer-card/offer-card-wide/OfferCardWide.styles.ts
@@ -0,0 +1,37 @@
+export const styles = {
+  appCard: {
+    p: '30px 20px'
+  },
+
+  wrapper: {
+    width: '100%',
+    display: 'flex',
+    position: 'relative',
+    gap: { sm: '24px', md: '40px' },
+    flexWrap: 'wrap',
+    borderRadius: '6px'
+  },
+
+  userInfo: {
+    root: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      gap: '4px',
+      maxWidth: '112px'
+    },
+    avatar: {
+      width: '80px',
+      height: '80px',
+      alignSelf: 'center',
+      mb: '12px'
+    }
+  },
+
+  buttons: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '16px',
+    pt: '30px'
+  }
+}
---
+++ src/components/offer-card/offer-card-wide/OfferCardWide.tsx
@@ -0,0 +1,87 @@
+import { Box } from '@mui/material'
+import { Offer } from '~/types'
+import { styles } from '~/components/offer-card/offer-card-wide/OfferCardWide.styles'
+
+import AppCard from '~/components/app-card/AppCard'
+import Bookmark from '~/components/offer-card/bookmark/Bookmark'
+import OfferDetails from '~/components/offer-card/offer-card-wide/offer-details/OfferDetails'
+import Price from '~/components/offer-card/price/Price'
+import SendMessageButton from '~/components/offer-card/buttons/SendMessageButton'
+import UserProfileInfo from '~/components/user-profile-info/UserProfileInfo'
+import ViewDetailsButton from '~/components/offer-card/buttons/ViewDetailsButton'
+
+interface OfferCardProps {
+  offer: Offer
+  onBookmarkClick: (id: string) => void
+  isBookmarked: boolean
+}
+
+const OfferCardWide: React.FC<OfferCardProps> = ({
+  offer,
+  onBookmarkClick,
+  isBookmarked
+}) => {
+  const {
+    _id,
+    title,
+    description,
+    languages,
+    price,
+    author,
+    authorRole,
+    subject,
+    category,
+    proficiencyLevel
+  } = offer
+
+  const userProfileInfoProps = {
+    _id: author._id,
+    firstName: author.firstName,
+    lastName: `${author.lastName[0]}.`,
+    photo: author.photo,
+    rating: author.averageRating[authorRole],
+    reviewsCount: author.totalReviews[authorRole],
+    role: authorRole,
+    sx: styles.userInfo
+  }
+  const offerDetailsProps = {
+    chipsColor: category.appearance.color,
+    description,
+    languages,
+    level: proficiencyLevel,
+    subject: subject.name,
+    title
+  }
+  const bookmarkProps = {
+    id: _id,
+    isBookmarked,
+    onBookmarkClick
+  }
+
+  // TODO: add chat opening logic
+  const handleChatOpening = () => {}
+
+  return (
+    <AppCard sx={styles.appCard}>
+      <Box sx={styles.wrapper}>
+        <UserProfileInfo {...userProfileInfoProps} />
+
+        <OfferDetails {...offerDetailsProps} />
+
+        <Box>
+          <Price price={price} />
+
+          <Box sx={styles.buttons}>
+            <ViewDetailsButton />
+
+            <SendMessageButton onClick={handleChatOpening} />
+          </Box>
+        </Box>
+
+        <Bookmark {...bookmarkProps} />
+      </Box>
+    </AppCard>
+  )
+}
+
+export default OfferCardWide

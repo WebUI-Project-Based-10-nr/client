988cc601d0f0eacbe8eb96473d27172d34d9d75e
Annaivv <annashalya@gmail.com>
2024-11-07 15:19:35 +0200

    notification modal is implemented on SignupForm submit

--- src/containers/guest-home-page/notification-modal/NotificationModal.tsx
+++ src/containers/guest-home-page/notification-modal/NotificationModal.tsx
@@ -4,6 +4,7 @@ import { Box } from '@mui/material'
 import ImgTitleDescription from '~/components/img-title-description/ImgTitleDescription'
 import AppButton from '~/components/app-button/AppButton'
 import { styles } from '~/containers/guest-home-page/notification-modal/NotificationModal.styles'
+import PopupDialog from '~/components/popup-dialog/PopupDialog'
 
 interface ConfirmEmailModal {
   description: string | ReactElement
@@ -21,15 +22,23 @@ const NotificationModal: FC<ConfirmEmailModal> = ({
   onClose
 }) => {
   return (
-    <Box sx={styles.root}>
-      <ImgTitleDescription
-        description={description}
-        img={img}
-        style={styles.imgTitleDesc}
-        title={title}
-      />
-      <AppButton onClick={onClose}>{buttonTitle}</AppButton>
-    </Box>
+    <PopupDialog
+      closeModalAfterDelay={onClose}
+      closeOnly
+      content={
+        <Box sx={styles.root}>
+          <ImgTitleDescription
+            description={description}
+            img={img}
+            style={styles.imgTitleDesc}
+            title={title}
+          />
+          <AppButton onClick={onClose}>{buttonTitle}</AppButton>
+        </Box>
+      }
+      paperProps={{}}
+      timerId={null}
+    />
   )
 }
 

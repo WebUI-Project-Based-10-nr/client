21fa21a745b482a4f566fce6b4d0bde419f55609
Kateryna Bartienieva <k7bart@gmail.com>
2024-11-12 14:35:22 +0200

    update

--- src/pages/categories/Categories.tsx
+++ src/pages/categories/Categories.tsx
@@ -5,26 +5,25 @@ import useBreakpoints from '~/hooks/use-breakpoints'
 import useCategoriesNames from '~/hooks/use-categories-names'
 import useLoadMore from '~/hooks/use-load-more'
 
-import { itemsLoadLimit } from '~/constants'
 import { authRoutes } from '~/router/constants/authRoutes'
-import { CategoryInterface } from '~/types'
 import { categoryService } from '~/services/category-service'
 import { getScreenBasedLimit } from '~/utils/helper-functions'
+import { itemsLoadLimit } from '~/constants'
+import { categoryIcons } from '~/constants/icon-map'
 
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
 import AppToolbar from '~/components/app-toolbar/AppToolbar'
-import DirectionLink from '~/components/direction-link/DirectionLink'
-
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
+import CardsList from '~/components/cards-list/CardsList'
+import CardWithLink from '~/components/card-with-link/CardWithLink'
 import CreateSubjectModal from '~/containers/find-offer/create-new-subject/CreateNewSubject'
+import DirectionLink from '~/components/direction-link/DirectionLink'
 import NotFoundResults from '~/components/not-found-results/NotFoundResults'
 import OfferRequestBlock from '~/containers/find-offer/offer-request-block/OfferRequestBlock'
 import PageWrapper from '~/components/page-wrapper/PageWrapper'
-
-import CardWithLink from '~/components/card-with-link/CardWithLink'
-import CardsList from '~/components/cards-list/CardsList'
-import { categoryIcons } from '~/constants/icon-map'
-import TitleWithDescription from '~/components/title-with-description/TitleWithDescription'
 import SearchAutocomplete from '~/components/search-autocomplete/SearchAutocomplete'
+import TitleWithDescription from '~/components/title-with-description/TitleWithDescription'
+
+import { CategoryInterface } from '~/types'
 
 import palette from '~/styles/app-theme/app.pallete'
 import { styles } from '~/pages/categories/Categories.styles'
@@ -37,16 +36,11 @@ const Categories = () => {
   const { t } = useTranslation()
   const { openModal } = useModalContext()
 
-  const [searchQuery, setSearchQuery] = useState('')
-  const params = useMemo(() => ({ name: searchQuery }), [searchQuery]) // for useLoadMore hook, maybe change ???
-
   const breakpoints = useBreakpoints()
   const cardsLimit = getScreenBasedLimit(breakpoints, itemsLoadLimit)
 
-  const getCategoryNames = () => {
-    if (!categoriesNames.length) void fetchData()
-    return categoriesNames
-  }
+  const [searchQuery, setSearchQuery] = useState('')
+  const params = useMemo(() => ({ name: searchQuery }), [searchQuery])
 
   const {
     loading: categoriesNamesLoading,
@@ -56,15 +50,20 @@ const Categories = () => {
 
   const {
     data: categories,
-    loading: categoriesLoading, // for CardsList and for check
-    // resetData,
-    loadMore, // for CardsList
-    isExpandable // for CardsList
+    loading: categoriesLoading,
+    resetData,
+    loadMore,
+    isExpandable
   } = useLoadMore<CategoryInterface, Pick<CategoryInterface, 'name'>>({
     service: categoryService.getCategories,
     limit: cardsLimit,
     params
   })
+
+  const getCategoryNames = () => {
+    if (!categoriesNames.length) void fetchData()
+    return categoriesNames
+  }
 
   const options = categoriesNames.map((category) => category.name)
 
@@ -104,6 +103,7 @@ const Categories = () => {
   return (
     <PageWrapper>
       <OfferRequestBlock />
+
       <DirectionLink
         after={arrow}
         linkTo={authRoutes.findOffers.path}
@@ -120,6 +120,7 @@ const Categories = () => {
         <SearchAutocomplete
           loading={categoriesNamesLoading}
           onFocus={getCategoryNames}
+          onSearchChange={resetData}
           options={options}
           search={searchQuery}
           setSearch={setSearchQuery}
@@ -127,7 +128,7 @@ const Categories = () => {
         />
       </AppToolbar>
 
-      {(!categories.length && !categoriesLoading) || categories.length === 0 ? (
+      {!categories.length && !categoriesLoading ? (
         <NotFoundResults
           buttonText={t('errorMessages.buttonRequest', {
             name: 'category'

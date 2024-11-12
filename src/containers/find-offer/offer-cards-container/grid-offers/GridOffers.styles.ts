eaad98caaded9a20657e17f256be68c38878b649
Kateryna Bartienieva <k7bart@gmail.com>
2024-11-12 20:03:38 +0200

    create GridOffers component, add styles

---
+++ src/containers/find-offer/offer-cards-container/grid-offers/GridOffers.styles.ts
@@ -0,0 +1,11 @@
+export const styles = {
+  container: {
+    display: 'grid',
+    gap: '24px',
+
+    gridTemplateColumns: {
+      xs: '1fr',
+      md: 'repeat(3, 1fr)'
+    }
+  }
+}
---
+++ src/containers/find-offer/offer-cards-container/grid-offers/GridOffers.tsx
@@ -0,0 +1,25 @@
+import { Box } from '@mui/material'
+import OfferCardNarrow from '~/components/offer-card/offer-card-narrow/OfferCardNarrow'
+import { Offer } from '~/types'
+import { styles } from '~/containers/find-offer/offer-cards-container/grid-offers/GridOffers.styles'
+
+interface GridOffersProps {
+  offers: Offer[]
+}
+
+const GridOffers: React.FC<GridOffersProps> = ({ offers }) => (
+  <Box sx={styles.container}>
+    {offers.map((offer) => {
+      return (
+        <OfferCardNarrow
+          isBookmarked
+          key={offer._id}
+          offer={offer}
+          onBookmarkClick={() => {}}
+        />
+      )
+    })}
+  </Box>
+)
+
+export default GridOffers

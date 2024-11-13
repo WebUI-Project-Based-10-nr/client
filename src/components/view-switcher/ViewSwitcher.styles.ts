03cd9bd094e4a616aa2c22da250fdd01a5e59fed
Kateryna Bartienieva <k7bart@gmail.com>
2024-11-13 19:41:34 +0200

    create ViewSwitcher component, add styles

---
+++ src/components/view-switcher/ViewSwitcher.styles.ts
@@ -0,0 +1,13 @@
+export const styles = {
+  container: {
+    width: 'fit-content',
+
+    '& > :nth-of-type(2)': {
+      marginLeft: '8px'
+    }
+  },
+
+  icon: {
+    color: 'primary.900'
+  }
+}

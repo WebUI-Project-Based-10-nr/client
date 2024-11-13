832b73b6d910f2a293615b5c79b6928cb977ad2f
Kateryna Bartienieva <k7bart@gmail.com>
2024-11-13 19:47:37 +0200

    add ViewSwitcher, add styles

---
+++ src/pages/find-offers/FindOffers.styles.ts
@@ -0,0 +1,3 @@
+export const styles = {
+  container: { display: 'flex', justifyContent: 'flex-end' }
+}
--- src/pages/find-offers/FindOffers.tsx
+++ src/pages/find-offers/FindOffers.tsx
@@ -1,7 +1,20 @@
+import { useState } from 'react'
+import { Box } from '@mui/material'
 import PageWrapper from '~/components/page-wrapper/PageWrapper'
+import ViewSwitcher from '~/components/view-switcher/ViewSwitcher'
+import { CardsViewMode } from '~/types/find-offers/findOffers.index'
+import { styles } from '~/pages/find-offers/FindOffers.styles'
 
 const FindOffers = () => {
-  return <PageWrapper>Find offers</PageWrapper>
+  const [viewMode, setViewMode] = useState<CardsViewMode>('list')
+
+  return (
+    <PageWrapper>
+      <Box sx={styles.container}>
+        <ViewSwitcher onChange={setViewMode} viewMode={viewMode} />
+      </Box>
+    </PageWrapper>
+  )
 }
 
 export default FindOffers

6d2fa09fcba24b186373b761e7523499c344f679
Kateryna Bartienieva <k7bart@gmail.com>
2024-11-14 17:12:51 +0200

    enshure that ViewSwitcher shows only on large screens

--- src/pages/find-offers/FindOffers.tsx
+++ src/pages/find-offers/FindOffers.tsx
@@ -1,17 +1,21 @@
 import { useState } from 'react'
 import { Box } from '@mui/material'
+import useBreakpoints from '~/hooks/use-breakpoints'
 import PageWrapper from '~/components/page-wrapper/PageWrapper'
 import ViewSwitcher from '~/components/view-switcher/ViewSwitcher'
-import { CardsViewMode } from '~/types/find-offers/findOffers.index'
+import { CardsViewMode } from '~/types'
 import { styles } from '~/pages/find-offers/FindOffers.styles'
 
 const FindOffers = () => {
+  const { isLaptopAndAbove } = useBreakpoints()
   const [viewMode, setViewMode] = useState<CardsViewMode>('list')
 
   return (
     <PageWrapper>
       <Box sx={styles.container}>
-        <ViewSwitcher onChange={setViewMode} viewMode={viewMode} />
+        {isLaptopAndAbove && (
+          <ViewSwitcher onChange={setViewMode} viewMode={viewMode} />
+        )}
       </Box>
     </PageWrapper>
   )
